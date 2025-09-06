f404f84fddfd21d349c6909bcc48e5a66f549852
niceu <niceusantos1365@gmail.com>
2025-09-06 11:31:18 -0300

    refactor: remoção de arquivos padrão do vue

--- frontend/frontend-app/src/App.vue
+++ frontend/frontend-app/src/App.vue
@@ -1,22 +1,9 @@
+
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
@@ -38,16 +25,11 @@ nav {
   margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
 nav a.router-link-exact-active:hover {
   background-color: transparent;
 }
 
 nav a {
-  display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
 }
