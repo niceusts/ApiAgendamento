b684ea1acba697b205b6c44634a35ca32abb5222
niceu <niceusantos1365@gmail.com>
2025-09-08 21:13:47 -0300

    feat and refactor: adicionei rota de agendas de um medico, corrigi erra no excluir de horario

--- ApiAgendamento/Domain/Entities/Medico.cs
+++ ApiAgendamento/Domain/Entities/Medico.cs
@@ -3,8 +3,8 @@ namespace ApiAgendamento.Domain.Entities;
 public class Medico
 {
     public int Id { get; private set; }
-    public string Nome { get; private set; }
-    public string Especialidade { get; private set; }
+    public string? Nome { get; private set; }
+    public string? Especialidade { get; private set; }
 
     public List<HorarioDisponivel> HorariosDisponiveis { get; private set; } = new();
 
